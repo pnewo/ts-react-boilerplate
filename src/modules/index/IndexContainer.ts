73317bf1e023840004b988bedfcbb1a7143af8ba
Lapanti <laurilavanti@gmail.com>
2017-04-26 07:30:04 +0300

    #29: Add WeatherComponent to IndexView

--- src/modules/index/IndexContainer.ts
+++ src/modules/index/IndexContainer.ts
@@ -1,16 +1,18 @@
 import { bindActionCreators, Dispatch } from 'redux';
 import { connect } from 'react-redux';
 import { State, Actions } from '../../redux/reducer';
-import { setTitle, saveTodo, setDone } from './IndexReducer';
+import { getWeather, setTitle, saveTodo, setDone } from './IndexReducer';
 import IndexView, { IIndexProps } from './IndexView';
 
 const stateToProps = (state: State) => ({
     title: state.index.title,
     todos: state.index.todos,
     loading: state.index.loading,
+    weather: state.index.weather,
 });
 
 const dispatchToProps = (dispatch: Dispatch<Actions>) => ({
+    getWeather: bindActionCreators(getWeather, dispatch),
     setTitle: bindActionCreators(setTitle, dispatch),
     saveTodo: bindActionCreators(saveTodo, dispatch),
     setDone: bindActionCreators(setDone, dispatch),
--- src/modules/index/IndexView.tsx
+++ src/modules/index/IndexView.tsx
@@ -2,23 +2,36 @@ import * as React from 'react';
 import { Dispatch } from 'redux';
 import { Actions } from '../../redux/reducer';
 import Todo from '../../common/Todo';
+import WeatherComponent from '../../components/WeatherComponent';
 import TodoComponent from '../../components/TodoComponent';
 import Button from '../../components/Button';
 import Loader from '../../components/Loader';
 
 export interface IIndexProps {
-    title: string;
-    todos: Todo[];
-    loading: boolean;
+    readonly title: string;
+    readonly todos: Todo[];
+    readonly loading: boolean;
+    readonly weather?: string;
+    getWeather: (lat: number, lon: number) => void;
     setTitle: (n: string) => Dispatch<Actions>;
     saveTodo: () => Dispatch<Actions>;
     setDone: (i: number) => Dispatch<Actions>;
 }
 
-const IndexView: React.StatelessComponent<IIndexProps> = ({ title, todos, loading, setTitle, saveTodo, setDone }) => (
+const IndexView: React.StatelessComponent<IIndexProps> = ({
+    title,
+    todos,
+    loading,
+    weather,
+    getWeather,
+    setTitle,
+    saveTodo,
+    setDone,
+}) => (
     <main className="index">
         {loading && <Loader />}
         <h1 className="index__header">Todo app</h1>
+        <WeatherComponent weather={weather} getWeather={getWeather} />
         <form className="index__form" onSubmit={e => e.preventDefault()}>
             <label className="index__form__label" htmlFor="newtodo">Add a new todo:</label>
             <input
